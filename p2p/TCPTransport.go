33d6c522689e440221ce222dc9e59220942d8eda
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-08-18 01:54:51 +0530

    newtcppeer && tests

--- p2p/TCPTransport.go
+++ p2p/TCPTransport.go
@@ -1,9 +1,22 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
+
+type TCPPeer struct {
+	conn net.Conn
+	outbound bool
+}
+
+func NewTCPPeer(conn net.Conn, outbound bool) *TCPPeer {
+	return &TCPPeer{
+		conn: conn,
+		outbound: outbound,
+	}
+}
 
 type TCPTransport struct {
 	listenAddress string
@@ -13,10 +26,35 @@ type TCPTransport struct {
 }
 
 func NewTCPTransport(listenAddr string) *TCPTransport {
-  return &TCPTransport{
-    listenAddress: listenAddr,
-  }
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
 }
 
+func (t *TCPTransport) ListenAndAccept() error {
+	var err error
 
+	t.listener, err = net.Listen("tcp", t.listenAddress)
+	if err != nil {
+		return err
+	}
 
+	go t.startAcceptLoop()
+	return nil
+
+}
+
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %s\n", err)
+		}
+
+		go t.handleConn(conn)
+	}
+}
+
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("New incoming connection from %s\n", conn.RemoteAddr())
+}
